5f5101b42736ea95f5ff7eb90297214a0f10f31a
harshitcodex0 <harshit456987@hotmail.com>
2024-05-13 18:31:30 +0530

    ...

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
+$(document).ready(function () {
+  let buttonColors = ["red", "blue", "green", "yellow"];
+  let gamePattern = [];
+  let userClickPattern = [];
 
-let buttonColors = ["red", "blue", "green", "yellow"]
-let gamePattern = [];
-
-function nextSequence(){
+  function nextSequence() {
     let randomChosenColor = buttonColors[Math.floor(Math.random() * 4)];
     gamePattern.push(randomChosenColor);
 
-    let audio = new Audio("sounds/"+randomChosenColor+".mp3");
+    let audio = new Audio("sounds/" + randomChosenColor + ".mp3");
     audio.play();
-}
+  }
 
-nextSequence();
-console.log(gamePattern)
+  nextSequence();
+  console.log(gamePattern);
 
+  $("." + gamePattern)
+    .animate({ opacity: 0.5 })
+    .animate({ opacity: 1 });
 
-$(document).ready(function(){
-    $("."+gamePattern).animate({opacity:0.5}).animate({opacity:1});
-    $("."+gamePattern).play
-})
+  $(".btn").click(function () {
+    let userChosenColor = $(this).attr("id");
+    userClickPattern.push(userChosenColor);
+    console.log(userClickPattern);
+  });
+});
